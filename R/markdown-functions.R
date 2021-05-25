c82c3dea3c14d48e94477e4d279fb617c588fcc7
an583 <anoori1@mgh.harvard.edu>
2021-05-25 12:29:33 -0700

    Updated show_table function.

--- R/markdown-functions.R
+++ R/markdown-functions.R
@@ -1,13 +1,14 @@
 #' Show Markdown table.
 #'
-#' Read table in Markdown document. Depends on `knitr` and `kableExtra` packages.
+#' Read table in Markdown document. Depends on \code{knitr} and \code{kableExtra} packages.
 #'
 #' @param table Table to display.
-#' @return `kable` table object.
+#' @param height Optional parameter to set height of scroll box. Default is 300px.
+#' @return \code{kable} table object.
 #' @author Ayush Noori
 #' @export
-show_table = function(table) {
+show_table = function(table, height = "300px") {
   kable(table, row.names = FALSE, align = "c") %>%
     kable_styling(bootstrap_options = c("striped", "bordered", "hover", "responsive"), font_size=12)  %>%
-    scroll_box(width = "100%", height = "300px", fixed_thead = list(enabled = T, background = "#159957"), extra_css = "margin-bottom: 1.2em;")
+    scroll_box(width = "100%", height = height, fixed_thead = list(enabled = T, background = "#159957"), extra_css = "margin-bottom: 1.2em;")
 }
