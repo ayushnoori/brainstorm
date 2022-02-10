8604f13d022e9c3d6866f7005a67265718e8bc00
Ayush Noori <anoori1@mgh.harvard.edu>
2022-02-10 15:59:55 -0500

    Updated dependencies.

--- R/stats-functions.R
+++ R/stats-functions.R
@@ -15,18 +15,18 @@ summarize_model = function(model, label = NULL) {
 
   # compute CIs
   my_confint = confint(model) %>%
-    as.data.table(keep.rownames = "Comparison")
+    data.table::as.data.table(keep.rownames = "Comparison")
 
   # model results
   model_results = my_summary$coefficients %>%
-    as.data.table(keep.rownames = "Comparison") %>%
-    merge(my_confint, by = "Comparison") %>%
-    setnames(c("2.5 %", "97.5 %"), c("Lower CI", "Upper CI"))
+    data.table::as.data.table(keep.rownames = "Comparison") %>%
+    data.table::merge(my_confint, by = "Comparison") %>%
+    data.table::setnames(c("2.5 %", "97.5 %"), c("Lower CI", "Upper CI"))
 
   if(!is.null(label)) {
     model_results = model_results %>%
       .[, Label := ..label] %>%
-      setcolorder(c("Label", "Comparison"))
+      data.table::setcolorder(c("Label", "Comparison"))
   }
 
   # return model results
